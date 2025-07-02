d9025c81adcc27a7a21c528c37d54b196499e118
SeRViP <121581935+EvGRaF87@users.noreply.github.com>
2025-07-02 12:30:37 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -132,9 +132,9 @@ chmod 700 -R "$SHORTCUT_DIR"
 
 echo -e "${BLUE}Создаем файл ярлыка: $SHORTCUT_FILE...${RESET}"
 {
-    echo "#!/bin/bash"
-    echo "bash $B_SH_PATH"
-} > "$SHORTCUT_FILE"
+    cp "$B_SH_PATH"
+} "$SHORTCUT_FILE"
+
 
 chmod +x "$SHORTCUT_FILE"
 echo -e "${GREEN}Ярлык 'FindeReal' успешно создан!${RESET}"
